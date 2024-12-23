27447f6872b30484a37b23c65f713fb2359bc945
wzyoung <741292199@qq.com>
2024-12-23 13:41:48 +0800

    fanyi

---
+++ demo/fanyi/lan.js
@@ -0,0 +1,578 @@
+export default [
+    {
+        englishName: "auto",
+        chineseName: "自动识别",
+        code: "auto"
+    },
+
+    {
+        englishName: "Chinese",
+        chineseName: "简体中文",
+        code: "zh-CHS"
+    },
+    {
+        englishName: "Chinese",
+        chineseName: "繁体中文",
+        code: "zh-CHT"
+    },
+    {
+        englishName: "Japanese",
+        chineseName: "日语",
+        code: "ja"
+    },
+    {
+        englishName: "Korean",
+        chineseName: "韩语",
+        code: "ko"
+    },
+    {
+        englishName: "Arabic",
+        chineseName: "阿拉伯语",
+        code: "ar"
+    },
+    {
+        englishName: "German",
+        chineseName: "德语",
+        code: "de"
+    },
+    {
+        englishName: "English",
+        chineseName: "英语",
+        code: "en"
+    },
+    {
+        englishName: "Spanish",
+        chineseName: "西班牙语",
+        code: "es"
+    },
+    {
+        englishName: "French",
+        chineseName: "法语",
+        code: "fr"
+    },
+    {
+        englishName: "Hindi",
+        chineseName: "印地语",
+        code: "hi"
+    },
+    {
+        englishName: "Indonesian",
+        chineseName: "印度尼西亚语",
+        code: "id"
+    },
+    {
+        englishName: "Italian",
+        chineseName: "意大利语",
+        code: "it"
+    },
+    {
+        englishName: "Dutch",
+        chineseName: "荷兰语",
+        code: "nl"
+    },
+    {
+        englishName: "Portuguese",
+        chineseName: "葡萄牙语",
+        code: "pt"
+    },
+    {
+        englishName: "Russian",
+        chineseName: "俄语",
+        code: "ru"
+    },
+    {
+        englishName: "hai",
+        chineseName: "泰语",
+        code: "th"
+    },
+    {
+        englishName: "Vietnamese",
+        chineseName: "越南语",
+        code: "vi"
+    },
+    {
+        englishName: "Afrikaans",
+        chineseName: "南非荷兰语",
+        code: "af"
+    },
+    {
+        englishName: "Amharic",
+        chineseName: "阿姆哈拉语",
+        code: "am"
+    },
+    {
+        englishName: "Azeerbaijani",
+        chineseName: "阿塞拜疆语",
+        code: "az"
+    },
+    {
+        englishName: "Belarusian",
+        chineseName: "白俄罗斯语",
+        code: "be"
+    },
+    {
+        englishName: "Bulgarian",
+        chineseName: "保加利亚语",
+        code: "bg"
+    },
+    {
+        englishName: "Bangla",
+        chineseName: "孟加拉语",
+        code: "bn"
+    },
+    {
+        englishName: "Bosnian (Latin)",
+        chineseName: "波斯尼亚语",
+        code: "bs"
+    },
+    {
+        englishName: "Catalan",
+        chineseName: "加泰隆语",
+        code: "ca"
+    },
+    {
+        englishName: "Cebuano",
+        chineseName: "宿务语",
+        code: "ceb"
+    },
+    {
+        englishName: "Corsican",
+        chineseName: "科西嘉语",
+        code: "co"
+    },
+    {
+        englishName: "Czech",
+        chineseName: "捷克语",
+        code: "cs"
+    },
+    {
+        englishName: "Welsh",
+        chineseName: "威尔士语",
+        code: "cy"
+    },
+    {
+        englishName: "Danish",
+        chineseName: "丹麦语",
+        code: "da"
+    },
+    {
+        englishName: "Greek",
+        chineseName: "希腊语",
+        code: "el"
+    },
+    {
+        englishName: "Esperanto",
+        chineseName: "世界语",
+        code: "eo"
+    },
+    {
+        englishName: "Estonian",
+        chineseName: "爱沙尼亚语",
+        code: "et"
+    },
+    {
+        englishName: "Basque",
+        chineseName: "巴斯克语",
+        code: "eu"
+    },
+    {
+        englishName: "Persian",
+        chineseName: "波斯语",
+        code: "fa"
+    },
+    {
+        englishName: "Finnish",
+        chineseName: "芬兰语",
+        code: "fi"
+    },
+    {
+        englishName: "Fijian",
+        chineseName: "斐济语",
+        code: "fj"
+    },
+    {
+        englishName: "Frisian",
+        chineseName: "弗里西语",
+        code: "fy"
+    },
+    {
+        englishName: "Irish",
+        chineseName: "爱尔兰语",
+        code: "ga"
+    },
+    {
+        englishName: "Scots",
+        chineseName: "苏格兰盖尔语",
+        code: "gd"
+    },
+    {
+        englishName: "Galician",
+        chineseName: "加利西亚语",
+        code: "gl"
+    },
+    {
+        englishName: "Gujarati",
+        chineseName: "古吉拉特语",
+        code: "gu"
+    },
+    {
+        englishName: "Hausa",
+        chineseName: "豪萨语",
+        code: "ha"
+    },
+    {
+        englishName: "Hawaiian",
+        chineseName: "夏威夷语",
+        code: "haw"
+    },
+    {
+        englishName: "Hebrew",
+        chineseName: "希伯来语",
+        code: "he"
+    },
+    {
+        englishName: "Hindi",
+        chineseName: "印地语",
+        code: "hi"
+    },
+    {
+        englishName: "Croatian",
+        chineseName: "克罗地亚语",
+        code: "hr"
+    },
+    {
+        englishName: "Haitian",
+        chineseName: "海地克里奥尔语",
+        code: "ht"
+    },
+    {
+        englishName: "Hungarian",
+        chineseName: "匈牙利语",
+        code: "hu"
+    },
+    {
+        englishName: "Armenian",
+        chineseName: "亚美尼亚语",
+        code: "hy"
+    },
+    {
+        englishName: "Igbo",
+        chineseName: "伊博语",
+        code: "ig"
+    },
+    {
+        englishName: "Icelandic",
+        chineseName: "冰岛语",
+        code: "is"
+    },
+    {
+        englishName: "Javanese",
+        chineseName: "爪哇语",
+        code: "jw"
+    },
+    {
+        englishName: "Georgian",
+        chineseName: "格鲁吉亚语",
+        code: "ka"
+    },
+    {
+        englishName: "Kazakh",
+        chineseName: "哈萨克语",
+        code: "kk"
+    },
+    {
+        englishName: "Khmer",
+        chineseName: "高棉语",
+        code: "km"
+    },
+    {
+        englishName: "Kannada",
+        chineseName: "卡纳达语",
+        code: "kn"
+    },
+    {
+        englishName: "Kurdish",
+        chineseName: "库尔德语",
+        code: "ku"
+    },
+    {
+        englishName: "Kyrgyz",
+        chineseName: "柯尔克孜语",
+        code: "ky"
+    },
+    {
+        englishName: "Latin",
+        chineseName: "拉丁语",
+        code: "la"
+    },
+    {
+        englishName: "Luxembourgish",
+        chineseName: "卢森堡语",
+        code: "lb"
+    },
+    {
+        englishName: "Lao",
+        chineseName: "老挝语",
+        code: "lo"
+    },
+    {
+        englishName: "Lithuanian",
+        chineseName: "立陶宛语",
+        code: "lt"
+    },
+    {
+        englishName: "Latvian",
+        chineseName: "拉脱维亚语",
+        code: "lv"
+    },
+    {
+        englishName: "Malagasy",
+        chineseName: "马尔加什语",
+        code: "mg"
+    },
+    {
+        englishName: "Maori",
+        chineseName: "毛利语",
+        code: "mi"
+    },
+    {
+        englishName: "Macedonian",
+        chineseName: "马其顿语",
+        code: "mk"
+    },
+    {
+        englishName: "Malayalam",
+        chineseName: "马拉雅拉姆语",
+        code: "ml"
+    },
+    {
+        englishName: "Mongolian",
+        chineseName: "蒙古语",
+        code: "mn"
+    },
+    {
+        englishName: "Marathi",
+        chineseName: "马拉地语",
+        code: "mr"
+    },
+    {
+        englishName: "Malay",
+        chineseName: "马来语",
+        code: "ms"
+    },
+    {
+        englishName: "Maltese",
+        chineseName: "马耳他语",
+        code: "mt"
+    },
+    {
+        englishName: "Hmong",
+        chineseName: "白苗语",
+        code: "mww"
+    },
+    {
+        englishName: "Myanmar (Burmese)",
+        chineseName: "缅甸语",
+        code: "my"
+    },
+    {
+        englishName: "Nepali",
+        chineseName: "尼泊尔语",
+        code: "ne"
+    },
+    {
+        englishName: "Dutch",
+        chineseName: "荷兰语",
+        code: "nl"
+    },
+    {
+        englishName: "Norwegian",
+        chineseName: "挪威语",
+        code: "no"
+    },
+    {
+        englishName: "Nyanja (Chichewa)",
+        chineseName: "齐切瓦语",
+        code: "ny"
+    },
+    {
+        englishName: "Querétaro Otomi",
+        chineseName: "克雷塔罗奥托米语",
+        code: "otq"
+    },
+    {
+        englishName: "Punjabi",
+        chineseName: "旁遮普语",
+        code: "pa"
+    },
+    {
+        englishName: "Polish",
+        chineseName: "波兰语",
+        code: "pl"
+    },
+    {
+        englishName: "Pashto",
+        chineseName: "普什图语",
+        code: "ps"
+    },
+    {
+        englishName: "Romanian",
+        chineseName: "罗马尼亚语",
+        code: "ro"
+    },
+    {
+        englishName: "Sindhi",
+        chineseName: "信德语",
+        code: "sd"
+    },
+    {
+        englishName: "Sinhala (Sinhalese)",
+        chineseName: "僧伽罗语",
+        code: "si"
+    },
+    {
+        englishName: "Slovak",
+        chineseName: "斯洛伐克语",
+        code: "sk"
+    },
+    {
+        englishName: "Slovenian",
+        chineseName: "斯洛文尼亚语",
+        code: "sl"
+    },
+    {
+        englishName: "Samoan",
+        chineseName: "萨摩亚语",
+        code: "sm"
+    },
+    {
+        englishName: "Shona",
+        chineseName: "修纳语",
+        code: "sn"
+    },
+    {
+        englishName: "Somali",
+        chineseName: "索马里语",
+        code: "so"
+    },
+    {
+        englishName: "Albanian",
+        chineseName: "阿尔巴尼亚语",
+        code: "sq"
+    },
+    {
+        englishName: "Serbian (Cyrillic)",
+        chineseName: "塞尔维亚语(西里尔文)",
+        code: "sr-Cyrl"
+    },
+    {
+        englishName: "Serbian (Latin)",
+        chineseName: "塞尔维亚语(拉丁文)",
+        code: "sr-Latn"
+    },
+    {
+        englishName: "Sesotho",
+        chineseName: "塞索托语",
+        code: "st"
+    },
+    {
+        englishName: "Sundanese",
+        chineseName: "巽他语",
+        code: "su"
+    },
+    {
+        englishName: "Swedish",
+        chineseName: "瑞典语",
+        code: "sv"
+    },
+    {
+        englishName: "Kiswahili",
+        chineseName: "斯瓦希里语",
+        code: "sw"
+    },
+    {
+        englishName: "Tamil",
+        chineseName: "泰米尔语",
+        code: "ta"
+    },
+    {
+        englishName: "Telugu",
+        chineseName: "泰卢固语",
+        code: "te"
+    },
+    {
+        englishName: "Tajik",
+        chineseName: "塔吉克语",
+        code: "tg"
+    },
+    {
+        englishName: "Filipino",
+        chineseName: "菲律宾语",
+        code: "tl"
+    },
+    {
+        englishName: "Klingon",
+        chineseName: "克林贡语",
+        code: "tlh"
+    },
+    {
+        englishName: "Tongan",
+        chineseName: "汤加语",
+        code: "to"
+    },
+    {
+        englishName: "Turkish",
+        chineseName: "土耳其语",
+        code: "tr"
+    },
+    {
+        englishName: "Tahitian",
+        chineseName: "塔希提语",
+        code: "ty"
+    },
+    {
+        englishName: "Ukrainian",
+        chineseName: "乌克兰语",
+        code: "uk"
+    },
+    {
+        englishName: "Urdu",
+        chineseName: "乌尔都语",
+        code: "ur"
+    },
+    {
+        englishName: "Uzbek",
+        chineseName: "乌兹别克语",
+        code: "uz"
+    },
+    {
+        englishName: "Xhosa",
+        chineseName: "南非科萨语",
+        code: "xh"
+    },
+    {
+        englishName: "Yiddish",
+        chineseName: "意第绪语",
+        code: "yi"
+    },
+    {
+        englishName: "Yoruba",
+        chineseName: "约鲁巴语",
+        code: "yo"
+    },
+    {
+        englishName: "Yucatec",
+        chineseName: "尤卡坦玛雅语",
+        code: "yua"
+    },
+    {
+        englishName: "Cantonese (Traditional)",
+        chineseName: "粤语",
+        code: "yue"
+    },
+    {
+        englishName: "Zulu",
+        chineseName: "南非祖鲁语",
+        code: "zu"
+    }
+]
