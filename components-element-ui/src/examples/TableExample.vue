0a2f570cc61b5b923e0d2a12ca7c1ea5c5b3a800
wzy <741292199@qq.com>
2025-03-11 17:06:42 +0800

    x

--- components-element-ui/src/examples/TableExample.vue
+++ components-element-ui/src/examples/TableExample.vue
@@ -2,7 +2,7 @@
   <MySpace direction="vertical" class="table-example" style="background-color: #fff; padding: 50px 10px">
     <MyFullScreen ref=".table-example" style="margin-left: auto" />
     <MyCard class="w-100">
-      <MyTable ref="tableRef" :data="tableData" :columns="columns" row-key="id" default-expand-all :span-method="spanMethod">
+      <MyTable border ref="tableRef" :data="tableData" :columns="columns" row-key="id" default-expand-all :span-method="spanMethod">
         <template #default="{ row, column }">
           <!-- <template v-if="column.value === 'name'">
             <el-tag>{{ row.name }}</el-tag>
@@ -15,7 +15,7 @@
       </MyTable>
     </MyCard>
     <MyCard class="w-100" style="height: 300px">
-      <MyTable   row-key="id" size="small" ref="tableRef2" height="100%" :data="tableData2" :columns="columns">
+      <MyTable row-key="id" size="small" ref="tableRef2" height="100%" :data="tableData2" :columns="columns">
         <template #header="{ column, $index }">
           <template v-if="$index === 0">
             {{ column.label }}
@@ -44,21 +44,42 @@ export default {
       tableData: [
         {
           id: 1,
-          name: '张三张三张三张三张三张三张三张三张三',
+          name: '张三',
           age: 18,
           address: '上海市浦东新区',
           phone: '13800138000',
         },
         {
           id: 2,
-          name: '张三张三张三张三张三张三张三张三张三',
+          name: '张三',
           age: 18,
           address: '上海市浦东新区',
           phone: '13800138001',
         },
         {
           id: 3,
-          name: '王五',
+          name: '张三',
+          age: 25,
+          address: '上海市浦东新区',
+          phone: '13800138002',
+        },
+        {
+          id: 4,
+          name: '张三',
+          age: 25,
+          address: '上海市浦东新区',
+          phone: '13800138002',
+        },
+        {
+          id: 5,
+          name: '李四',
+          age: 25,
+          address: '上海市浦东新区',
+          phone: '13800138002',
+        },
+        {
+          id: 6,
+          name: '李四',
           age: 25,
           address: '上海市浦东新区',
           phone: '13800138002',
@@ -137,8 +158,8 @@ export default {
       console.log('删除', row);
     },
     spanMethod({ row, rowIndex, column }) {
-      const standardColumn = 'name';
-      const mergeColumns = ['name', 'address'];
+      // 数组的顺序存在=>合并顺序和依赖顺序（例：age基于name合并, address基于name和age合并）
+      const mergeColumns = ['name', 'age', 'address'];
 
       // 如果是序号列或不在合并列中,返回默认值
       if (column.type === 'index' || !mergeColumns.includes(column.property)) {
@@ -147,9 +168,10 @@ export default {
 
       const property = column.property;
       const prevRow = this.tableData[rowIndex - 1];
+      const columnIndex = mergeColumns.indexOf(property);
 
       // 检查是否需要与上一行合并
-      const shouldMergeWithPrev = prevRow && prevRow[property] === row[property] && (property === standardColumn || prevRow[standardColumn] === row[standardColumn]);
+      const shouldMergeWithPrev = prevRow && prevRow[property] === row[property] && mergeColumns.slice(0, columnIndex).every((col) => prevRow[col] === row[col]);
 
       if (shouldMergeWithPrev) {
         return { rowspan: 0, colspan: 0 };
@@ -159,7 +181,7 @@ export default {
       let mergeCount = 1;
       for (let i = rowIndex + 1; i < this.tableData.length; i++) {
         const nextRow = this.tableData[i];
-        const canMerge = nextRow[property] === row[property] && (property === standardColumn || nextRow[standardColumn] === row[standardColumn]);
+        const canMerge = nextRow[property] === row[property] && mergeColumns.slice(0, columnIndex).every((col) => nextRow[col] === row[col]);
 
         if (!canMerge) break;
         mergeCount++;
